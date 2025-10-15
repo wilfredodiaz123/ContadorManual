0928817be6e6b5bcc532bad56f57766a853ddb88
Wilfred O Steven Diaz Ortiz <dabosswill666@gmail.com>
2025-10-15 10:48:15 -0600

    Agrega una clase especializada en contar

--- ContadorManual.GUI/MainPage.xaml.cs
+++ ContadorManual.GUI/MainPage.xaml.cs
@@ -1,27 +1,27 @@
-﻿namespace ContadorManual.GUI
+﻿using ContadorManual.GUI.Models;
+namespace ContadorManual.GUI
 {
     public partial class MainPage : ContentPage
     {
-        private int _conteo;
+        private Contador contador;
 
         public MainPage()
         {
             InitializeComponent();
-            _conteo = 0;
-            ConteoLabel.Text = _conteo.ToString();
-
+            contador = new Contador();
+            ConteoLabel.Text = contador.Conteo.ToString();
         }
 
-        private void Button_Clicked(object sender, EventArgs e)
+        private void OnContarButtonClicked(object sender, EventArgs e)
         {
-            _conteo++;
-            ConteoLabel.Text = _conteo.ToString();
+            contador.Contar();
+            ConteoLabel.Text = contador.Conteo.ToString();
         }
 
-        private void Button_Clicked_1(object sender, EventArgs e)
+        private void OnReiniciarButtonClicked(object sender, EventArgs e)
         {
-            _conteo=0;
-            ConteoLabel.Text = _conteo.ToString();
+            contador.Reiniciar();
+            ConteoLabel.Text = contador.Conteo.ToString();
         }
     }
 }
---
+++ ContadorManual.GUI/Models/Contador.cs
@@ -0,0 +1,35 @@
+﻿namespace ContadorManual.GUI.Models
+{
+    internal class Contador
+    {
+        private int _conteo;
+
+        public int Conteo
+        {
+            get => _conteo;
+            set
+            {
+                if (_conteo != value)
+                {
+                    _conteo = value;
+                }
+            }
+        }
+
+        public Contador()
+        {
+            Conteo = 0;
+        }
+
+        public void Contar()
+        {
+            Conteo++;
+        }
+
+        public void Reiniciar()
+        {
+            Conteo = 0;
+        }
+
+    }
+}
